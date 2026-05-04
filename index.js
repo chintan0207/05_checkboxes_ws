5839b01a5c9847f10f78187b182e3443303e15d6
chintan <imchintan0207@gmail.com>
2026-05-05 01:17:19 +0530

    feat: handle state on redis, new connected server in sync horizontal scaling

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import express from "express";
 import { Server } from "socket.io";
 import path from "node:path";
 
-import { publisher, subscriber } from "./redis-conection.js";
+import { publisher, redis, subscriber } from "./redis-conection.js";
 import { channel } from "node:diagnostics_channel";
 
 const CHECKBOX_COUNT = 200;
+const CHECKBOX_STATE_KEY = "checkbox-state";
 
 const state = {
   checkBoxes: new Array(CHECKBOX_COUNT).fill(false),
@@ -36,6 +37,17 @@ async function main() {
     socket.on("client:checkbox:change", async (data) => {
       console.log(`[socket: ${socket.id}]: client:checkbox:change:`, data);
 
+      const existingState = await publisher.get(CHECKBOX_STATE_KEY);
+
+      if (existingState) {
+        const remoteData = JSON.parse(existingState);
+        remoteData[data.index] = data.checked;
+        await redis.set(CHECKBOX_STATE_KEY, JSON.stringify(remoteData));
+      } else {
+        const checkBoxes = new Array(CHECKBOX_COUNT).fill(false);
+        await redis.set(CHECKBOX_STATE_KEY, JSON.stringify(checkBoxes));
+      }
+
       await publisher.publish(
         "internal-server:checkbox:change",
         JSON.stringify(data),
@@ -49,8 +61,13 @@ async function main() {
     res.status(200).json({ healthy: true });
   });
 
-  app.get(`/checkboxes`, (req, res) => {
-    return res.json({ checkboxes: state.checkBoxes });
+  app.get(`/checkboxes`, async(req, res) => {
+    const existingState = await redis.get(CHECKBOX_STATE_KEY);
+    if (existingState) {
+      const remoteData = JSON.parse(existingState);
+      return res.json({ checkboxes: remoteData });
+    }
+    return res.json({ checkboxes: new Array(CHECKBOX_COUNT).fill(false) });
   });
 
   server.listen(PORT, () => {
--- redis-conection.js
+++ redis-conection.js
@@ -1,11 +1,12 @@
-import redis from "ioredis";
+import redisClient from "ioredis";
 
 function createRedisConnection() {
-  return new redis({
+  return new redisClient({
     host: "localhost",
     port: 6379,
   });
 }
 
+export const redis = createRedisConnection()
 export const publisher = createRedisConnection();
 export const subscriber = createRedisConnection();
